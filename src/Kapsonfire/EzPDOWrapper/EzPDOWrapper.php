1f701e7c8aca4390470e9206c7cbbf46b6d258b2
Kapsonfire <kapsonfire@gmx.de>
2019-08-05 15:04:50 +0200

    Init

---
+++ src/Kapsonfire/EzPDOWrapper/EzPDOWrapper.php
@@ -0,0 +1,15 @@
+<?php
+namespace Kapsonfire\EzPDOWrapper;
+
+class EzPDOWrapper {
+    /**
+     * @var \PDO $db
+     */
+    private $db;
+    private function __construct()
+    {
+    }
+    public static function create(array $options) {
+
+    }
+}
