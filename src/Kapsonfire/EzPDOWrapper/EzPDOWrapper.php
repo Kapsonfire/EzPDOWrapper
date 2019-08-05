130c3d95eaab7859b00e19fd152d34146fe4d3e0
Kapsonfire <kapsonfire@gmx.de>
2019-08-05 15:45:52 +0200

    create/factory

--- src/Kapsonfire/EzPDOWrapper/EzPDOWrapper.php
+++ src/Kapsonfire/EzPDOWrapper/EzPDOWrapper.php
@@ -1,15 +1,69 @@
 <?php
+
 namespace Kapsonfire\EzPDOWrapper;
 
-class EzPDOWrapper {
+class EzPDOWrapper
+{
     /**
      * @var \PDO $db
      */
     private $db;
-    private function __construct()
-    {
-    }
-    public static function create(array $options) {
 
+    private function __construct(\PDO $pdoInstance)
+    {
+        $this->db = $pdoInstance;
+    }
+
+
+    /**
+     * Get the native PDO Instance
+     * @return \PDO
+     */
+    public function getPDO() : \PDO {
+        return $this->db;
+    }
+
+
+    public function __destruct()
+    {
+        $this->db = null;
+    }
+
+
+    /**
+     * @param string $database
+     * @param array $connectionInfo
+     * @param array $options
+     * @return EzPDOWrapper
+     */
+    public static function create(string $database, array $connectionInfo = [], array $options = []): EzPDOWrapper
+    {
+        $dsn = '';
+        try {
+            $instance = null;
+
+            $host       = $connectionInfo['host'] ?? 'localhost';
+            $port       = $connectionInfo['port'] ?? 3306;
+            $unix       = $connectionInfo['socket'] ?? null;
+            $user       = $connectionInfo['user'] ?? 'root';
+            $pass       = $connectionInfo['pass'] ?? '';
+            $charset    = $connectionInfo['charset'] ?? 'utf8';
+
+
+            $dsn = 'mysql:';
+            if ($unix === null) {
+                $dsn .= 'host = ' . $host . ';port = ' . $port . ';';
+            } else {
+                $dsn .= 'unix_socket=' . $unix . ';';
+            }
+            $dsn .= 'dbname=' . $database . ';charset=' . $charset;
+            $pdo = new \PDO($dsn, $user, $pass, $options);
+
+            $instance = new EzPDOWrapper($pdo);
+
+            return $instance;
+        } catch (\Exception $ex) {
+            throw new \PDOException('Can\'t connect to mysql database.');
+        }
     }
 }
